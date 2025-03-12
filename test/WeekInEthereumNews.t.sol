d90c774594942784529e4380900cd4074ab7e746
abcoathup <andrew@coathup.net>
2025-03-12 17:16:16 +1100

    add tests

--- test/WeekInEthereumNews.t.sol
+++ test/WeekInEthereumNews.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {WeekInEthereumNews} from "../src/WeekInEthereumNews.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {IERC721Errors} from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
 
 contract WeekInEthereumNewsTest is Test {
     WeekInEthereumNews public nft;
@@ -26,7 +27,7 @@ contract WeekInEthereumNewsTest is Test {
         assertEq(nft.owner(), owner);
     }
 
-    function testFuzz_Holder(uint256 tokenId) public view {
+    function testFuzz_OwnerOf(uint256 tokenId) public view {
         vm.assume(tokenId > 0 && tokenId <= nftSupply);
         assertEq(nft.ownerOf(tokenId), owner);
     }
@@ -34,5 +35,27 @@ contract WeekInEthereumNewsTest is Test {
     function testFuzz_TokenURI(uint256 tokenId) public view {
         vm.assume(tokenId > 0 && tokenId <= nftSupply);
         assertEq(nft.tokenURI(tokenId), string.concat("ipfs://[CID]/", Strings.toString(tokenId), ".json"));
+    }
+
+    function testFuzz_OwnerOf_NonexistentToken(uint256 nonexistentTokenId) public {
+        vm.assume(nonexistentTokenId < 1 || nonexistentTokenId > nftSupply);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IERC721Errors.ERC721NonexistentToken.selector,
+                nonexistentTokenId
+            )
+        );
+        nft.ownerOf(nonexistentTokenId);
+    }
+
+    function testFuzz_TokenURI_NonexistentToken(uint256 nonexistentTokenId) public {
+        vm.assume(nonexistentTokenId < 1 || nonexistentTokenId > nftSupply);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IERC721Errors.ERC721NonexistentToken.selector,
+                nonexistentTokenId
+            )
+        );
+        nft.tokenURI(nonexistentTokenId);
     }
 }
